677049c426bfab816952c8bf3d04c0bd3dcb30ed
DaniDeDos <hasashih978@gmail.com>
2024-02-23 09:22:11 -0500

    trabajando en servicios

--- src/components/pages/ServiceCard.jsx
+++ src/components/pages/ServiceCard.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const ServiceCard = ({ service }) => {
   const { id, icon: Icon, service_name, descripcion } = service;
 
   return (
-    <div key={id} className='service-card'>
-      <Icon />
+    <div key={id} className="service-card">
+      <div className="teste">
+        <div className="icon-container">
+          <Icon />
+        </div>
+      </div>
       <h3>{service_name}</h3>
       <p>{descripcion}</p>
-      
     </div>
   );
 };
