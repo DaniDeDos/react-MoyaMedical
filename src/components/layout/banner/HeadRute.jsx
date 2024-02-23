48fd4edd59f9088569d1759fc5ce7b3555f2381c
DaniDeDos <hasashih978@gmail.com>
2024-02-23 07:22:02 -0500

    trabajando en el header

--- src/components/layout/banner/HeadRute.jsx
+++ src/components/layout/banner/HeadRute.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+//importaciones de terceros
+import { GoHome } from "react-icons/go";
+
+//Importaciones propias
+import "../../../styles/headrute.css";
+
 const HeadRute = () => {
   return (
     <div className="rute-container">
-      <div className="rute-link">{"Home > Nuestros Servicios"}</div>
+      <div className="rute-link">
+        <GoHome />
+        <p>{"Home > Nuestros Servicios"}</p>
+      </div>
       <h1 className="title-rute">Nuestros Servicios</h1>
     </div>
   );
--- src/components/layout/banner/Header.jsx
+++ src/components/layout/banner/Header.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "../../../Style.css";
+import "../../../styles/navbar.css";
 import list_ico from "../../../assets/icons/list.svg";
 import HeadRute from "./HeadRute";
 import Background from "./Background";
 import NavMenuItem from "./NavMenuItem";
-import { GoHome } from "react-icons/go";
+import { SiImmich } from "react-icons/si";
 
 const Header = () => {
   const [navMenuVisible, setNavMenuVisible] = useState(false);
@@ -18,11 +19,17 @@ const Header = () => {
       <Background />
       <header className="header">
         <nav className="nav">
-        <GoHome />
-          <a href="#" className="logo nav-link">
-            <span className="span">Moya</span>
-            Medical
-          </a>
+          <div className="logo-container">
+            <div className="nav-logo">
+              <SiImmich />
+            </div>
+
+            <a href="#" className="logo nav-link">
+              <span className="span">Moya</span>
+              Medical
+            </a>
+          </div>
+
           <button
             className="toggle nav-toggle"
             aria-label={navMenuVisible ? "Cerrar Menu" : "Abrir Menu"}
