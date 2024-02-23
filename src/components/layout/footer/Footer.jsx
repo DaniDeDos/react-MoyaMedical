14f8ce201bbfefe08055f75a2e4a9959065a9f17
DaniDeDos <hasashih978@gmail.com>
2024-02-23 10:24:08 -0500

    trabajando el footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,25 +1,34 @@
 import NavMenuItem from "../banner/NavMenuItem.jsx";
+import { SiImmich } from "react-icons/si";
 
 const Footer = () => {
   const derechoEs = "© 2024 Moya Medical. Todos los derechos reservados.";
 
   return (
-    <footer className="footer">
-      <nav className="nav">
-        <a href="#" className="logo nav-link">
-          <span className="span">Moya</span>
-          Medical
-        </a>
-        <ul className={`nav-menu `}>
-          <NavMenuItem name="Nuestra Compañía" />
-          <NavMenuItem name="Nuestros Servicios" />
-          <NavMenuItem name="Recursos" />
-          <NavMenuItem name="ES" />
-          <NavMenuItem name="Contáctanos" />
-        </ul>
-      </nav>
-      <p className="derechos">{derechoEs}</p>
-    </footer>
+    <>
+      <footer className="footer">
+        <nav className="nav">
+          <div className="logo-container">
+            <div className="nav-logo">
+              <SiImmich />
+            </div>
+
+            <a href="#" className="logo nav-link">
+              <span className="span">Moya</span>
+              Medical
+            </a>
+          </div>
+          <ul className={`nav-menu `}>
+            <NavMenuItem name="Nuestra Compañía" />
+            <NavMenuItem name="Nuestros Servicios" />
+            <NavMenuItem name="Recursos" />
+            <NavMenuItem name="ES" />
+            <NavMenuItem name="Contáctanos" />
+          </ul>
+        </nav>
+        <p className="derechos">{derechoEs}</p>
+      </footer>
+    </>
   );
 };
 
